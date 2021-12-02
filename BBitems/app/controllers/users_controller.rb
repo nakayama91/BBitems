e9db7d62ba1b2ff727cfbecfff938445a16d3038
nakayama91 <k.naka10ry@gmail.com>
2021-12-02 08:49:58 +0000

    finish_MVC

---
+++ BBitems/app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def leave
+  end
+
+end
--- BBitems/config/routes.rb
+++ BBitems/config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #ホーム画面ルーティング
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  
+  #userルーティング
+  resources :users, only:[:show, :edit, :update, :destroy] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  get 'customers/leave' => 'customers#leave'
+  
+  #itemルーティング
+  resources :items do
+    resource :item_comments, only:[:create, :destroy]
+    resource :likes, only: [:create, :destroy]
+  end
+  
+  #genreルーティング
+  resources :genre, only:[:index, :edit, :create, :update, :destroy]
+  
 end
