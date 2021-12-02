9f4c5062512e5fd47d0f2bf272586445595a89a1
nakayama91 <k.naka10ry@gmail.com>
2021-12-02 10:31:08 +0000

    新規登録完了

--- BBitems/app/controllers/application_controller.rb
+++ BBitems/app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    #新規登録時に、userへ追加したカラム情報の取得を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #カスタマー情報編集時に、userへ追加したカラム情報の取得を許可
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction, :image_id])
+  end
+
+  #ログイン後の遷移先
+  def after_sign_in_path_for(resource)
+    current_user
+  end
+
+  #新規登録後の遷移先
+  def after_sign_up_path_for(_resource)
+    current_user
+  end
+
+  #ユーザー情報変更後の遷移先
+  def after_update_path_for(resource)
+    current_user
+  end
+
+
 end
--- BBitems/app/models/user.rb
+++ BBitems/app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :items,         dependent: :destroy
   has_many :item_comments, dependent: :destroy
   has_many :likes,         dependent: :destroy
-  
+
   # 自分がフォローする（与フォロー）側の関係性（userはたくさんの人をフォローしている）
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
-  
+
   # 自分がフォローされる（被フォロー）側の関係性（userはたくさんの人にフォローされている）
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 被フォロー関係を通じて参照→自分をフォローしている人
@@ -29,9 +29,9 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
+
   attachment :profile_image
-  
-  validates :name, length: { maximum: 20 }, uniqueness: true
-         
+
+  validates :name, presence: true,  length: { maximum: 20 }, uniqueness: true
+
 end
