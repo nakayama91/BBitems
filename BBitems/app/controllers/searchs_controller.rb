92d4f62aae156082a641b0266a994a1f2c223f66
nakayama91 <k.naka10ry@gmail.com>
2021-12-12 09:21:23 +0000

    fix_search

--- BBitems/app/controllers/searchs_controller.rb
+++ BBitems/app/controllers/searchs_controller.rb
@@ -1,8 +1,9 @@
 class SearchsController < ApplicationController
 
   def index #部分検索
-    @items = Item.where("name LIKE ? ", '%' + params[:search] + '%').order(created_at: :desc) #製品名にて検索可能
-    @items = Item.where("maker LIKE ? ", '%' + params[:search] + '%').order(created_at: :desc) #メーカー名にて検索可能
+    @items = Item.where("name LIKE ?", '%' + params[:search] + '%').
+             or(Item.where("maker LIKE ?", '%' + params[:search] + '%')).
+             order(created_at: :desc) #製品名にて検索可能
     @genres = Genre.all
   end
 
