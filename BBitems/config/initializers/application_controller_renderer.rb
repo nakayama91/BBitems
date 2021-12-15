57e24eccaafe69017842d62b52c8aef078456783
nakayama91 <k.naka10ry@gmail.com>
2021-12-15 11:27:38 +0000

    refile_key

--- BBitems/config/initializers/application_controller_renderer.rb
+++ BBitems/config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = '70dddf546a2027ecf344da2e9f66cdcbf7f1d1cdb72b812aee4dd5ce42a5db09967ee39b7c51e3a6464ca3d47006d2cf93e484178a29787a64abed7f6c12128b'
