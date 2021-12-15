464bbe08a3a8f9d68bf5fb97a1b68e9617dc2180
nakayama91 <k.naka10ry@gmail.com>
2021-12-15 13:48:06 +0000

    add_seed.rb

--- BBitems/db/seeds.rb
+++ BBitems/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  name: ENV['DB_ADMINNAME'],
+  email: ENV['DB_EMAIL'],
+  admin: true,
+  password: ENV['DB_PASSWORD'],
+  )
