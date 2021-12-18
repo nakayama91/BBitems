66e33a94be5e35b42d56b6e8767d96b51e9d297a
nakayama91 <k.naka10ry@gmail.com>
2021-12-18 07:23:30 +0000

    item削除リンク確認

--- BBitems/spec/system/items_spec.rb
+++ BBitems/spec/system/items_spec.rb
@@ -5,20 +5,22 @@ require 'rails_helper'
 describe '投稿のテスト' do
   let!(:item) { create(:item, name:'name',introduction:'---')}
 
-  describe 'index画面(items_path)のテスト' do
+  # 1.top画面
+  describe 'top画面(root_path)のテスト' do
     before do
-      visit items_path
+      visit root_path
     end
     context '表示の確認' do
-      it 'index画面(items_path)に「投稿一覧」が表示されているか' do
-        expect(page).to have_content '投稿一覧'
+      it 'top画面(root_path)に「Welcom to BBitems」が表示されているか' do
+        expect(page).to have_content 'Welcom to BBitems'
       end
-      it 'items_pathが"items"であるか' do
-        expect(current_path).to eq('/items')
+      it 'root_pathが"/"であるか' do
+        expect(current_path).to eq('/')
       end
     end
   end
 
+  # 2.新規投稿画面
   describe "投稿画面(new_item_path)のテスト" do
     before do
       user = FactoryBot.create(:user)
@@ -32,12 +34,41 @@ describe '投稿のテスト' do
       it '投稿ボタンが表示されているか' do
         expect(page).to have_content '投稿する'
       end
-      context '投稿処理のテスト' do
-        it '投稿後のリダイレクト先は正しいか' do
-          fill_in 'item[name]', with: Faker::Lorem.characters(number:5)
-          fill_in 'item[introduction]', with: Faker::Lorem.characters(number:20)
-          click_button '投稿する'
-          expect(page).to have_current_path items_path
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'item[name]', with: Faker::Lorem.characters(number:5)
+        fill_in 'item[introduction]', with: Faker::Lorem.characters(number:20)
+        click_button '投稿する'
+        expect(page).to have_current_path items_path
+      end
+    end
+  end
+
+  # 3.投稿一覧のテスト
+  describe "投稿一覧のテスト" do
+    before do
+      visit items_path
+    end
+    context '表示の確認' do
+      it '投稿されたものが表示されているか' do
+        expect(page).to have_content item.name
+        expect(page).to have_link item.name
+      end
+    end
+  end
+
+  # 4.詳細画面のテスト
+  describe "詳細画面のテスト" do
+    before do
+      @user = FactoryBot.create(:user)
+      sign_in @user
+      visit item_path(item.id)
+    end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        if item.user_id == @user.id
+          expect(page).to have_link '削除する'
         end
       end
     end
