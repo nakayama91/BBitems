6461bf340dfc5fd6e737bdebfc0a4b691e9e832d
nakayama91 <k.naka10ry@gmail.com>
2021-12-21 07:36:25 +0000

    mail_subject

--- BBitems/app/mailers/inquiry_mailer.rb
+++ BBitems/app/mailers/inquiry_mailer.rb
@@ -2,8 +2,8 @@ class InquiryMailer < ApplicationMailer
 
   def received_email(inquiry)
     @inquiry = inquiry
-    mail(:to => inquiry.email, :subject => 'お問い合わせを承りました')
-    mail bcc: ENV['INQUIRE_MAILER_ADDRESS']
+    mail(:to => inquiry.email)
+    mail bcc: ENV['INQUIRE_MAILER_ADDRESS'], subject:'お問い合わせを承りました'
   end
 
 end
