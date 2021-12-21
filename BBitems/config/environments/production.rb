ca71bc9029bb24d29daffeabc808399176623c20
nakayama91 <k.naka10ry@gmail.com>
2021-12-21 07:13:22 +0000

    add_production.rb

--- BBitems/config/environments/production.rb
+++ BBitems/config/environments/production.rb
@@ -83,4 +83,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:             'smtp.gmail.com',
+    domain:              'gmail.com',
+    user_name:           ENV['INQUIRE_MAILER_ADDRESS'],
+    password:            ENV['INQUIRE_MAILER_PASSWORD'],
+    authentication:      'plain',
+    enable_starttls_auto: true
+    }
+
 end
